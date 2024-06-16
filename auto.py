32eff014b5a48d96315996467686f6304373db50
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-06-16 11:08:19 +0200

    Update

--- auto.py
+++ auto.py
@@ -50,24 +50,33 @@
 # except Exception as e:
 #     log_message(f"Erreur lors de l'ajout de la tâche à crontab : {str(e)}")
 
-import schedule
-import time
+import os
 import subprocess
+from datetime import datetime
 
-def execute_script():
+# Définir les chemins et paramètres
+SOURCE_DIR = "test.txt"
+DEST_DIR = "rakotondrasoa@192.168.48.131:/home/rakotondrasoa/Infra_et_SI/Backup/"
+LOG_FILE = "file.log"
+
+# Générer le nom de fichier de destination avec la date et l'heure actuelles
+now = datetime.now()
+timestamp = now.strftime("%d-%m-%Y_%H%M%S")
+dest_filename = f"test_{timestamp}.txt"
+dest_path = os.path.join(DEST_DIR, dest_filename)
+
+# Test de l'existence du fichier source
+if os.path.isfile(SOURCE_DIR):
+    # Commande scp pour transférer le fichier
     try:
-        subprocess.run(["python", "Backup.py"], check=True)
-        print("Script executed successfully")
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing script: {e}")
-
-# Planifier l'exécution du script tous les jours à 12h
-schedule.every().day.at("12:00").do(execute_script)
-
-# Planifier l'exécution du script toutes les heures
-schedule.every().hour.do(execute_script)
-
-# Boucle principale pour vérifier les planifications
-while True:
-    schedule.run_pending()
-    time.sleep(60)  # Attendre 60 secondes avant de vérifier à nouveau les planifications
+        subprocess.run(["scp", SOURCE_DIR, dest_path], check=True)
+        with open(LOG_FILE, "a") as log_file:
+            log_file.write(f"{datetime.now()} - Transfert terminé avec succès. Fichier transféré: {dest_filename}\n")
+    except subprocess.CalledProcessError:
+        with open(LOG_FILE, "a") as log_file:
+            log_file.write(f"{datetime.now()} - Erreur lors du transfert.\n")
+            exit(1)
+else:
+    with open(LOG_FILE, "a") as log_file:
+        log_file.write(f"{datetime.now()} - Le fichier source {SOURCE_DIR} n'existe pas.\n")
+        exit(1)
